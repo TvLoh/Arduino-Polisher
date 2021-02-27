043ab6017508e0663944c1e433c59570b8f2b885
T <fips.froelich@gmail.com>
2021-02-27 18:17:18 +0100

    Higher step pulse

--- oneStep.ino
+++ oneStep.ino
@@ -1,7 +1,7 @@
 // Stepper Driver detect the 4 Microsecond Puls as one step. As you kan see in the 
 void oneStep(int pin){
       digitalWrite(pin, HIGH);
-      delayMicroseconds(4);
+      delayMicroseconds(6);
       digitalWrite(pin, LOW);
       delayMicroseconds(4);
   }
